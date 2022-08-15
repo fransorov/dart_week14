f5a2e411ef135fe5c5f4306d64eaf914957a01a6
fransorov <franforker@gmail.com>
2022-08-15 11:13:58 +0700

    first commit

---
+++ bin/dart_week14.dart
@@ -0,0 +1,8 @@
+void main() {
+int sum = add(10,5,5,60,70);
+print(sum);
+}
+
+int add (int a,int b,int c,int d, int e){
+  return a*b+c*d+e;
+}
---
+++ bin/funtion2.dart
@@ -0,0 +1,9 @@
+void main() {
+  message("Mark", "57 / 9", "06512410");
+}
+
+void message(String name,String address,String phone) {
+  print("your name is $name");
+  print("Your address $address");
+  print("Your phone number is $phone");
+}
